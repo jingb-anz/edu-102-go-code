5862a50a2d380a591a0a646c2965e45e3f52a9a9
Brian Jing <153048309+jingb-anz@users.noreply.github.com>
2024-01-10 17:49:14 +1100

    Update workflow input

--- exercises/debug-activity/practice/start/main.go
+++ exercises/debug-activity/practice/start/main.go
@@ -71,10 +71,14 @@ func createPizzaOrder() *pizza.PizzaOrder {
 		Price:       1200,
 	}
 
-	// TODO: define a struct representing an additional pizza
+	// define a struct representing an additional pizza
+	p3 := pizza.Pizza{
+		Description: "Medium, with extra cheese",
+		Price:       1300,
+	}
 
-	// TODO: add the variable for that struct to this array
-	items := []pizza.Pizza{p1, p2}
+	// add the variable for that struct to this array
+	items := []pizza.Pizza{p1, p2, p3}
 
 	order := pizza.PizzaOrder{
 		OrderNumber: "Z1238",
