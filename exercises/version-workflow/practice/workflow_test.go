3f563741915dd82e07127bafdd794f5ae260bf41
Brian Jing <153048309+jingb-anz@users.noreply.github.com>
2024-01-10 18:16:46 +1100

    Add test for workflow replay

--- exercises/version-workflow/practice/workflow_test.go
+++ exercises/version-workflow/practice/workflow_test.go
@@ -2,19 +2,23 @@ package loanprocess
 
 import (
 	"testing"
-	// "github.com/stretchr/testify/assert"
-	// "go.temporal.io/sdk/worker"
+
+	"github.com/stretchr/testify/assert"
+	"go.temporal.io/sdk/worker"
 )
 
 func TestReplayWorkflowHistoryFromFile(t *testing.T) {
 
-	// TODO Create the Workflow Replayer
+	// Create the Workflow Replayer
+	replayer := worker.NewWorkflowReplayer()
 
-	// TODO Register the Workflow Definition with the Replayer
+	// Register the Workflow Definition with the Replayer
+	replayer.RegisterWorkflow(LoanProcessingWorkflow)
 
-	// TODO Replay the Event History from the JSON file you downloaded
+	// Replay the Event History from the JSON file you downloaded
+	err := replayer.ReplayWorkflowHistoryFromJSONFile(nil, "history_for_original_execution.json")
 
-	// TODO Use assert.NoError to verify that replaying the history
+	// Use assert.NoError to verify that replaying the history
 	//      does not return an error
-
+	assert.NoError(t, err)
 }
